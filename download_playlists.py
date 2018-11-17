d2513fbc7623cc9239c3c6a36094b7bdbe6ced84
apsvieira <apsvieira95@gmail.com>
2018-11-17 21:42:31 -0200

    Added workaround for downloads + adapted to directory structure

--- download_playlists.py
+++ download_playlists.py
@@ -1,8 +1,11 @@
 import argparse
+import logging
 import os
 from subprocess import Popen, PIPE
 
 import yaml
+
+import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--playlists',
@@ -22,11 +25,15 @@ parser.add_argument('--randomize',
 if __name__ == '__main__':
     opts = parser.parse_args()
     playlists_file = opts.playlists
-    max_per_list = opts.max_per_list
+    max_per_list = opts.max_per_list if opts.max_per_list is not None else None
     randomize = opts.randomize
 
+    logger = logging.getLogger("framesFromVideos")
+    logger.setLevel('DEBUG')
+
     # Construct argument list for youtube-dl from command line arguments
-    standard_commands = ["--yes-playlist", "--reject-title=\"\[Deleted video\]\""]
+    standard_commands = ["--yes-playlist", "--id", "--reject-title=\"\[Deleted video\]\"",
+                         "--min-sleep-interval=2", "--max-sleep-interval=7"]
     standard_commands.append('-s') if opts.simulate is not None else None
     standard_commands.append("--max-downloads={}".format(max_per_list))
     standard_commands.append("--playlist-random") if randomize else None
@@ -38,15 +45,21 @@ if __name__ == '__main__':
     # Transform Windows format path (\ separated) to POSIX-like (/ format). Remove line ending.
     youtube_dl = youtube_dl.replace('\\', '/').replace('\r\n', '')
 
+    data_path = os.path.abspath(os.path.join(os.curdir, 'data'))
+    videos_path = os.path.join(data_path, 'videos')
+    utils.create_folder(data_path)
+    utils.create_folder(videos_path)
+
     with open(playlists_file, 'r') as f:
         playlists = yaml.load(f)['playlists']
 
+    os.chdir(videos_path)
     for candidate, playlist in playlists.items():
-        playlist_path = os.path.relpath(candidate)
-        try:
-            os.mkdir(playlist_path)
-        except FileExistsError:
-            print("Tried to create a folder that already exists. Moving on without creating new folder.")
+        logger.info("Downloading videos for class {}.".format(candidate))
+        playlist_path = os.path.join(videos_path, candidate)
+        utils.create_folder(playlist_path)
 
         with Popen([youtube_dl, *standard_commands, playlist], stdout=PIPE, cwd=playlist_path) as p:
-            print(p.stdout.read())
+            out, err = p.communicate()
+            logger.info(out)
+            logger.error(err)
